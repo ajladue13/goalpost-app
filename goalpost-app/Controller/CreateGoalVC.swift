8845b96a8655d414f907a2b4eb9757703828b33b
Alex LaDue <aj.ladue@bellsouth.net>
2018-04-21 18:53:31 -0400

    - Built CreateGoal VC - Connected IB Actions and Outlets

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,39 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Alex LaDue on 4/21/18.
+//  Copyright © 2018 Alex LaDue. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    //Outlets
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //Actions
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
